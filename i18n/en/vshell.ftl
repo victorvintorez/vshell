bb609305a297dee276f6f95ddbdb3b93ae2ea0a2
victorvintorez <victorvintorez@proton.me>
2025-02-07 23:33:45 +0000

    start implementing template generation

--- i18n/en/vshell.ftl
+++ i18n/en/vshell.ftl
@@ -56,3 +56,7 @@ architecture-storage-redb_error_database-create-fail = Failed to create database
 
 architecture-theme-md3_expect_wallpaper-read = to read wallpaper file
 architecture-theme-md3_expect_image-read = to parse image file
+architecture-theme-template_info_templates-loading = Loading templates.
+architecture-theme-template_warn_template-not-found = Template for {$name} not found at {$path}
+architecture-theme-template_expect_template-target-dir = target to have parent directory
+architecture-theme-template_error_template-target-dir-fail = Failed to get target directory for template {$name}: {$path}
--- src/architecture/theme/template.rs
+++ src/architecture/theme/template.rs
@@ -1,5 +1,11 @@
 use crate::config::TemplateConfig;
+use crate::fl;
+use crate::info;
+use material_colors::theme::Theme;
 use std::collections::HashMap;
+use std::format;
+use std::path::Path;
+use tracing::warn;
 use upon::Engine;
 
 pub struct TemplateManager {
@@ -11,6 +17,50 @@ impl TemplateManager {
     pub fn new(templates: Option<HashMap<String, TemplateConfig>>) -> Self {
         let engine = init_template_engine();
         TemplateManager { templates, engine }
+    }
+
+    pub fn generate(&self, theme: &Theme) {
+        info!(
+            "{:?}",
+            &fl!("architecture-theme-template_info_templates-loading")
+        );
+
+        if let Some(templates) = &self.templates {
+            for (tmpl_name, template) in templates {
+                let template_path = Path::new(&template.template);
+                let target_path = Path::new(&template.target);
+
+                if !template_path.exists() {
+                    warn!(
+                        "{:?}",
+                        fl!(
+                            "architecture-theme-template_warn_template-not-found",
+                            name = tmpl_name,
+                            path = format!("{:?}", template_path)
+                        )
+                    );
+                    continue;
+                } else if !target_path
+                    .parent()
+                    .expect(&fl!(
+                        "architecture-theme-template_expect_template-target-dir"
+                    ))
+                    .exists()
+                {
+                    warn!(
+                        "{:?}",
+                        fl!(
+                            "architecture-theme-template_error_template-target-dir-fail",
+                            name = tmpl_name,
+                            path = format!("{:?}", target_path)
+                        )
+                    );
+                    continue;
+                }
+
+                // TODO: Implement template generation
+            }
+        }
     }
 }
 
